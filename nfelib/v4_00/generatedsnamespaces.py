efdb1ad8eb50712880227fe9b38a11c2f26fd33b
Hugo Borges <hugo.borges@kmee.com.br>
2020-02-21 16:28:43 -0300

    [ADD] generatednamespaces

--- nfelib/v4_00/generatedsnamespaces.py
+++ nfelib/v4_00/generatedsnamespaces.py
@@ -2,4 +2,9 @@
 GenerateDSNamespaceDefs = {
     "TEvento": 'xmlns="http://www.portalfiscal.inf.br/nfe"',
     "TNFe": 'xmlns="http://www.portalfiscal.inf.br/nfe"',
+    "SignatureType": 'xmlns="http://www.w3.org/2000/09/xmldsig#"',
+    "KeyInfoType": '',
+    "SignedInfoType": '',
+    "ReferenceType": '',
+    "TransformsType": '',
 }
